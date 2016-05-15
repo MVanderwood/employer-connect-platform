a5a78ef1ecfd71a59eb6c2ec59be2db066bbf829
Justin Jungst <jjungstprogramming@gmail.com>
2016-05-15 16:49:38 -0500

    employer edit and index pages started

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,2 +1,32 @@
 class EmployersController < ApplicationController
+  def index
+    @employers = Employer.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+    @employer = Employer.find(params[:id])
+
+  end
+
+  def update
+    @employer = Employer.find(params[:id])
+    @employer.update({
+    name: params[:name],
+    phone: params[:phone],
+    city: params[:city],
+    state: params[:state],
+    description: params[:description],
+    email_contact: params[:email_contact],
+    website: params[:website],
+    linkedin: params[:linkedin],
+    logo: params[:logo]})
+
+    redirect_to '/employers'
+
+  end
 end
